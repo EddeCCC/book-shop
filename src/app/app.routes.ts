0687bc1892706af08ea987bdd1013cbb4797382e
EddeCCC <eduard.schander@gmx.de>
2026-02-02 09:54:49 +0100

    add home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,40 @@
 import { Routes } from '@angular/router';
+import { Home } from './home/home';
+import { Books } from './books/books';
+import { BookDetails } from './book-details/book-details';
+import { Login } from './login/login';
+import { Checkout } from './checkout/checkout';
+import { Orders } from './orders/orders';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        title: 'Home Page',
+        component: Home
+    },
+    {
+        path: 'books',
+        title: 'Collection',
+        component: Books
+    },
+    {
+        path: 'books/:id',
+        title: 'Book Details',
+        component: BookDetails
+    },
+    {
+        path: 'login',
+        title: 'Login',
+        component: Login
+    },
+    {
+        path: 'checkout',
+        title: 'Checkout',
+        component: Checkout
+    },
+    {
+        path: 'orders',
+        title: 'Orders',
+        component: Orders
+    }
+];
