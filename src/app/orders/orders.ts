4861f79025f693abd30eff38960463b60d9e487c
EddeCCC <eduard.schander@gmx.de>
2026-02-03 16:23:13 +0100

    add orders page

--- src/app/orders/orders.ts
+++ src/app/orders/orders.ts
@@ -1,9 +1,10 @@
 import { Component, computed, inject } from '@angular/core';
 import { OrderService } from './order.service';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-orders',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './orders.html',
   styleUrl: './orders.scss',
 })
